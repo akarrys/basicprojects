34782785e3abb520bfe747d666ae4dd7e830bb4e
AK <artamiskarrys47@gmail.com>
2021-11-23 16:06:05 -0700

    Update main.java

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ package main;
 * @since   2021-09-23 
 **/
 
-public class main extends ExcerciseData  {  // extends main class to use ExerciseData class fields and methods, ..inheritance
+public class main extends ExcerciseData  {  // extends main class to use ExcerciseData class fields and methods, ..inheritance
 
 	public static void main(String[] args) {
 	main.inputAllBaseTypes(a,b,user,d,e,f,g,h);  // a call to inputAllBaseTypes method in ExerciseData class
