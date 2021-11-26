ddc2a8c4ebba99b31f3fc1e868d2d47769895a65
AK <artamiskarrys47@gmail.com>
2021-11-26 12:10:52 -0700

    Add files via upload

---
+++ isBuilder.java
@@ -0,0 +1,21 @@
+package main;
+import java.util.*;
+/*
+ * Short java program using a StringBuilder instance based on the substring static method to remove
+ * punctuation in a given String str sentence
+ * Author: AK
+ */
+
+public class isBuilder {
+	public static int beginIndex;  // making both int variables usable to other classes and main class
+	public static int endIndex;
+	
+
+	public static void main(String[] args) {  //main method
+		String str =  new String ("Let's try, Mike!");  //string builder instance
+		System.out.println(str.substring(0, 15));  // substring method to print from str index (0,15),
+		// TODO Auto-generated method stub
+
+	}
+
+}
