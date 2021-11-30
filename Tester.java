023da4256e92f729b88ae3e9bb5bf0e5a9f1f670
AK <artamiskarrys47@gmail.com>
2021-11-30 10:54:59 -0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,21 @@
+package main;
+import java.util.*;
+/*
+ * Small standalone program to print a sentence of a string array 100 times.
+ * Author: AK
+ * 
+ */
+
+public class Tester {
+
+	public static void main(String[] args) {
+		String[] strArray = {"I will never spam my friends again"};
+	 
+	   for( int i = 1; i <= 100; i++) {
+		   System.out.println(strArray[0]);
+	   }
+	    
+	
+	}
+
+}
